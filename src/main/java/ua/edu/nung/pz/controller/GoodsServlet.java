e0fd7a63094f07d168e91c6b9b04949200cd1493
Vladyslav Yatsushchak <vladoff36@gmail.com>
2024-06-13 21:30:01 +0300

    first commit

---
+++ src/main/java/ua/edu/nung/pz/controller/GoodsServlet.java
@@ -0,0 +1,55 @@
+package ua.edu.nung.pz.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import ua.edu.nung.pz.dao.entity.Good;
+import ua.edu.nung.pz.dao.repository.GoodRepository;
+import ua.edu.nung.pz.view.MainPage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+@WebServlet(name = "GoodsServlet", urlPatterns = {"/goods/*"})
+public class GoodsServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        GoodRepository goodRepository = new GoodRepository();
+        ArrayList<Good>  goods = goodRepository.getByBrand("Naturalis");
+
+        System.out.println(goods);
+
+        String body = goods.stream().map(good -> {
+            return "<div class=\"col-12 col-sm-6 col-lg-4 col-xl-3 my-2\">" +
+                    "<div class=\"card\" style=\"width: 18rem;\">\n" +
+                    "<img src=\"/img/" + (good.getPhoto().length > 0 ? good.getPhoto()[0] : "") + "\" class=\"card-img-top\" alt=\"good image\">" +
+                    "  <div class=\"card-body\">\n" +
+                    "    <h5 class=\"card-title\">" + good.getName() + "</h5>\n" +
+                    "    <h6 class=\"card-subtitle mb-2 text-body-secondary\">Price:" + good.getPrice().getFor_client() + " UAH</h6>\n" +
+                    "    <p class=\"card-text\">" + good.getShortDescription() + "</p>\n" +
+                    "    <a href=\"#\" class=\"card-link\">Card link</a>\n" +
+                    "    <a href=\"#\" class=\"card-link\">Another link</a>\n" +
+                    "  </div>\n" +
+                    "</div>"
+                    + "</div>";
+        }).collect(Collectors.joining());
+
+        body = "<div class=\"container-fluid\"> <div class=\"row\">" + body + "</div> </div>";
+
+        String builderPage = MainPage.Builder.newInstance()
+                .setTitle("Green Shop")
+                .setHeader("")
+                .setBody(body)
+                .setFooter()
+                .build()
+                .getFullPage();
+
+        out.println(builderPage);
+    }
+}
---
+++ src/main/java/ua/edu/nung/pz/controller/StartServlet.java
@@ -0,0 +1,120 @@
+package ua.edu.nung.pz.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import ua.edu.nung.pz.dao.entity.Firebase;
+import ua.edu.nung.pz.dao.entity.User;
+import ua.edu.nung.pz.dao.repository.UserRepository;
+import ua.edu.nung.pz.view.MainPage;
+import ua.edu.nung.pz.view.ViewConfig;
+
+import java.io.*;
+import java.util.Properties;
+
+
+@WebServlet(name = "StartServlet", urlPatterns = {"/*"}, loadOnStartup = 1)
+public class StartServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String context = "";
+        HttpSession httpSession = request.getSession();
+        User user = (User) httpSession.getAttribute(User.USER_SESSION_NAME);
+        String userName = user == null ? "" : user.getDisplayName();
+
+        switch (request.getPathInfo()) {
+            case "/contacts":
+                context = "<h2>Our Contacts!</h2>\n";
+                break;
+            case "/forgotpassword":
+                context = "<h2>Restore Password!</h2>\n";
+                break;
+            default:
+                context = "<h2>Hello World from Servlet!</h2>\n";
+        }
+
+        String builderPage = MainPage.Builder.newInstance()
+                .setTitle("Green Shop")
+                .setHeader(userName)
+                .setBody(context)
+                .setFooter()
+                .build()
+                .getFullPage();
+
+        out.println(builderPage);
+
+        // TODO remove test code
+        UserRepository userRepository = new UserRepository();
+        User user1 = userRepository.getUserByEmail("nafta@nung");
+        System.out.println(user1);
+
+
+//        user.setEmail("email1@email.com");
+//        user.setPassword("112211221122");
+//        user.setDisplayName("Test User");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String contextPath = request.getContextPath();
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        HttpSession httpSession;
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+
+        // login / register
+        Firebase firebase = Firebase.getInstance();
+        if (firebase.getUserByEmail(user.getEmail()).equals(Firebase.USER_EXISTS)) {
+            String firebaseResponse = firebase.signInWithEmailAndPassword(user.getEmail(), user.getPassword());
+            if(firebaseResponse.equals(Firebase.PASSWORD_OK)) {
+                System.out.println(Firebase.PASSWORD_OK);
+                user.setDisplayName("Best User");
+                httpSession = request.getSession();
+                httpSession.setAttribute(User.USER_SESSION_NAME, user);
+            }  else {
+                System.out.println("Wrong Password");
+            }
+        } else {
+            System.out.println("User NOT Exist");
+            String userMsg = Firebase.getInstance().createUser(user);
+        }
+
+        System.out.println(user);
+
+        response.sendRedirect("/");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        String pathBuilder = getServletContext().getRealPath("htmlBuilder/");
+
+        ViewConfig viewConfig = ViewConfig.getInstance();
+        viewConfig.setPath(pathBuilder);
+
+        initFirebase();
+    }
+
+    private void initFirebase() {
+        Properties props = new Properties();
+        InputStream is = getClass().getClassLoader().getResourceAsStream("app.properties");
+        try {
+            props.load(is);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Firebase.getInstance().setFirebaseConfigPath(props.getProperty("file.path"));
+        Firebase.getInstance().setFirebaseName(props.getProperty("firebase.name"));
+        Firebase.getInstance().setApiKey(props.getProperty("web.api.key"));
+        Firebase.getInstance().setSignInUrl(props.getProperty("signInUrl"));
+        Firebase.getInstance().init();
+    }
+}
